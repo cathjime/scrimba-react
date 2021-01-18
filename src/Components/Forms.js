832efb6f7a264843ed65cf509aa992cf983cc388
cathjime <xcatherine.jimenez@gmail.com>
2021-01-18 17:59:12 -0500

    added dropdown

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -5,6 +5,8 @@ class Forms extends React.Component {
     firstName: "",
     lastName: "",
     isFriendly: false,
+    gender: "",
+    faveColor: "sea green",
   };
 
   handleChange = (e) => {
@@ -35,30 +37,57 @@ class Forms extends React.Component {
             placeholder="Last Name"
             onChange={this.handleChange}
           />
+
+          <textarea value={"A random value"} onChange={this.handleChange} />
+          <br />
+          <label>
+            <input
+              type="checkbox"
+              name="isFriendly"
+              checked={this.state.isFriendly}
+              onChange={this.handleChange}
+            />{" "}
+            Is Friendly?
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="female"
+              checked={this.state.gender === "female"}
+              onChange={this.handleChange}
+            />{" "}
+            Female
+          </label>
+          <br />
+          <label>
+            <input
+              type="radio"
+              name="gender"
+              value="male"
+              checked={this.state.gender === "male"}
+              onChange={this.handleChange}
+            />{" "}
+            Male
+          </label>
+          <br />
+          <label>Favorite Color</label>
+          <select
+            value={this.state.faveColor}
+            onChange={this.handleChange}
+            name="faveColor"
+          >
+            <option value="dusty rose">Dusty Rose</option>
+            <option value="powder blue">Powder Blue</option>
+            <option value="sea green">Sea Green</option>
+          </select>
+
           <h2>
             {this.state.firstName} {this.state.lastName}
           </h2>
-          <input type="submit" label="Submit" />
+          <h3>You are a : {this.state.gender}</h3>
         </form>
-        {/* <textarea value={"A random value"} /> */}
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="isFriendly"
-            checked={this.state.isFriendly}
-            onChange={this.handleChange}
-          />{" "}
-          Is Friendly?
-        </label>
-        <br />
-        <label>
-          <input type="radio" name="gender" value="female" /> Female
-        </label>
-        <br />
-        <label>
-          <input type="radio" name="gender" value="male" /> Male
-        </label>
       </>
     );
   }
